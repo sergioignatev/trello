be40b0899d7e5b82e5a81917a5279e94f1a751cd
sergioIgnatev <sergioignatev17@gmail.com>
2023-03-27 23:01:03 +0300

    h

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,12 @@ let services=()=>{
     return `
     <select id="first" onchange='time()' >
     <option value=''>Выберите пункт</option>
-    <option id='one' value="one">ТЕХОСМОТР Борзова 58а т. 92-13-46</option>
+    <option id='one' value="Борзова 58а">ТЕХОСМОТР Борзова 58а т. 92-13-46</option>
     <option  id='two'value="two">ТЕХОСМОТР Фридриха 15 т. 92-13-23</option>
     </select>
     `
 }
+document.querySelector("#demo").innerHTML=services()
 let datetime=()=>{
     return `<input id='date' type='date'>
     <select id="datetime__time">
@@ -38,6 +39,7 @@ function myF(){
 document.querySelector('#time').addEventListener('click',time)
 function time(){
 let f=document.getElementById('first');
+
 document.querySelector('#time').checked==true
 if(f.value!==""){
     document.getElementById('demo').innerHTML=datetime()
@@ -45,4 +47,12 @@ if(f.value!==""){
     document.getElementById('demo').innerHTML='ВЫберите услугу'
 }
 
+}
+setTimeout(Check,1000)
+function Check(){
+    let first=document.getElementById('first');
+    let secont=document.getElementById('date');
+    
+    document.querySelector('.place').innerHTML=first.value
+    console.log(first.value)
 }
