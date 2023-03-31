62878ca07f2d44961da7a94323324b9f572517b1
sergioIgnatev <sergioignatev17@gmail.com>
2023-03-31 21:14:29 +0300

    k

--- scripts/main.js
+++ scripts/main.js
@@ -1,58 +1,21 @@
-document.querySelector('#service').addEventListener('click',myF)
+document.querySelector('#test').addEventListener('mouseover',Send);
+let json={}
+function Send(){
+    let place=document.getElementById('place').value;
+    let date=document.getElementById('date').value;
+    let time=document.getElementById('time').value;
+    let name=document.getElementById('name').value;
+    let textarea=document.getElementById('textarea').value;
+    if(place!=='' && date!=='' && time!=="" && name!==''){
+        document.getElementById('demo').innerHTML=place+' '+date+' '+time+" "+name+" "+textarea;
 
-let services=()=>{
-    return `
-    <select id="first" onchange='time()' >
-    <option value=''>Выберите пункт</option>
-    <option id='one' value="Борзова 58а">ТЕХОСМОТР Борзова 58а т. 92-13-46</option>
-    <option  id='two'value="two">ТЕХОСМОТР Фридриха 15 т. 92-13-23</option>
-    </select>
-    `
-}
-document.querySelector("#demo").innerHTML=services()
-let datetime=()=>{
-    return `<input onchange="Check" id='date' type='date'>
-    <select id="datetime__time">
-    <option value=''>Выберите время</option>
-    <option value="09:00">09:00</option>
-    <option value="09:30">09:30</option>
-    <option value="10:00">10:00</option>
-    <option value="10:30">10:30</option>
-
-
-
-
-
-    </select>
-    `
-
-}
-
-function myF(){
-    let h=document.getElementById('service')
-    if(h.checked===true){
-        document.getElementById('demo').innerHTML=services()
-    }else{
-        document.getElementById('demo').innerHTML=''
+       json.name=name;
+       json.place=place;
+       json.time=time;
+       json.textarea=textarea;
+       json.date=date
+       console.log(json)
     }
-}
-document.querySelector('#time').addEventListener('click',time)
-function time(){
-let f=document.getElementById('first');
-
-
-if(f.value!==""){
-    document.getElementById('demo').innerHTML=datetime()
-}else if(f.value===""){
-    document.getElementById('demo').innerHTML='ВЫберите услугу'
-}
-
-}
-setInterval(Check,1)
-function Check(){
-  let f=document.querySelector('#datetime__time')
-    let s=document.getElementById('date');
-    
-    document.querySelector('.place').innerHTML=f.value+" "+s.value
-    
+  
+  
 }
